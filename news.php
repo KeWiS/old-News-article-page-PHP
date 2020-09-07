188bf47824885030053cfa34c1981b5bffe9f10c
Michał Siwek <mjzsiwek@gmail.com>
2020-09-07 17:21:35 +0200

    Added site wireframe

---
+++ news.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8" >
+        <title>News article site</title>
+        <link rel = "stylesheet" type = "text/css" href = "style.css" >
+    </head>
+    <body>
+        <header>
+            <h2>News article prototype site</h2>
+        </header>
+    </body>
+</html>
