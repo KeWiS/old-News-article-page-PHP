f3c0ae661ede236ea0ff81d03173417c61252bcb
Michał Siwek <mjzsiwek@gmail.com>
2020-09-07 17:53:29 +0200

    Added article authors list and code cleanup

--- news.php
+++ news.php
@@ -7,7 +7,29 @@
     </head>
     <body>
         <header>
-            <h2>News article prototype site</h2>
+            <h1>News article prototype site</h1>
         </header>
+        <section class = "authors">
+            <h2>Article authors:</h2>
+            <ul>
+                <?php
+                    //Connection variables
+                    $host = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $database = "news";
+                    //Connection variable
+                    $connect = mysqli_connect($host, $username, $password, $database);
+                    //Query for authors list variable
+                    $query = mysqli_query($connect, "SELECT id, name, surname from authors");
+                    //Reading lines returned from database
+                    while($row = mysqli_fetch_array($query)){
+                        echo "<li>$row[id]. $row[name] $row[surname]</li>";
+                    }
+                    //Closing connection
+                    mysqli_close($connect);
+                ?>
+            </ul>
+        </section>
     </body>
 </html>
