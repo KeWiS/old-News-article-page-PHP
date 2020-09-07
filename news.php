94f2e074522527bbf7d83c78e73d682c1ac2fc5d
Michał Siwek <mjzsiwek@gmail.com>
2020-09-07 18:08:28 +0200

    Added articles list, changed list types

--- news.php
+++ news.php
@@ -11,7 +11,7 @@
         </header>
         <section class = "authors">
             <h2>Article authors:</h2>
-            <ul>
+            <ol>
                 <?php
                     //Connection variables
                     $host = "localhost";
@@ -21,15 +21,37 @@
                     //Connection variable
                     $connect = mysqli_connect($host, $username, $password, $database);
                     //Query for authors list variable
-                    $query = mysqli_query($connect, "SELECT id, name, surname from authors");
+                    $query = mysqli_query($connect, "SELECT name, surname from authors");
                     //Reading lines returned from database
                     while($row = mysqli_fetch_array($query)){
-                        echo "<li>$row[id]. $row[name] $row[surname]</li>";
+                        echo "<li>$row[name] $row[surname]</li>";
                     }
                     //Closing connection
                     mysqli_close($connect);
                 ?>
-            </ul>
+            </ol>
+        </section>
+        <section class = "articles">
+            <h2>Articles list:</h2>
+            <ol>
+                <?php
+                    //Connection variables
+                    $host = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $database = "news";
+                    //Connection variable
+                    $connect = mysqli_connect($host, $username, $password, $database);
+                    //Query for authors list variable
+                    $query = mysqli_query($connect, "SELECT title from articles");
+                    //Reading lines returned from database
+                    while($row = mysqli_fetch_array($query)){
+                        echo "<li>$row[title]</li>";
+                    }
+                    //Closing connection
+                    mysqli_close($connect);
+                ?>
+            </ol>
         </section>
     </body>
 </html>
